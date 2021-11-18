bccdf41fad0cf0ea446f47275ab07f749a2846a1
Keiddy <keiddygarcia@gmail.com>
2021-11-17 20:43:25 -0500

    [feat]: Canciones

--- src/canciones/canciones.controllers.js
+++ src/canciones/canciones.controllers.js
@@ -1,5 +1,6 @@
 const cancionesService = require('./canciones.service')
 const jwt = require("jsonwebtoken")
+const request = require("request");
 
 const cancionesController = {}
 
@@ -7,6 +8,32 @@ cancionesController.obtenerNombres = async (req, res) =>  {
     try {
         const obtenerNombres = await cancionesService.obtenerNombres()
         return res.status(200).json(obtenerNombres)
+    } catch (error) {
+        res.send(error)
+    }
+}
+
+cancionesController.reproducirCancion = async (req, res) =>  {
+    try {
+        let options = { 
+            method: 'POST',
+            url: 'https://st-cantio.devout.cc/',
+            headers:
+            { 
+                'Cache-Control': 'no-cache',
+                'Content-Type': 'multipart/form-data' 
+            },
+            formData: { 
+                token_n: 'fQViD^trC6L*shivlCPneIiI0zDK9Aajc3H5mEU',
+                name: req.body.nombre,
+                song: req.body.cancion,
+                Preview: req.body.preview
+            } 
+        };
+        request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+            return res.status(200).json(response.body)
+        });
     } catch (error) {
         res.send(error)
     }
--- src/canciones/canciones.routes.js
+++ src/canciones/canciones.routes.js
@@ -4,6 +4,7 @@ const cancionesController = require('./canciones.controllers')
 const db = require('../database');
 
 router.get('/obtenerNombres', cancionesController.obtenerNombres);
+router.post('/reproducirCanciones', cancionesController.reproducirCancion);
 
 
 module.exports = router;
