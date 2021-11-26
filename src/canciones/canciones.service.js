077a659efe23a867e1affae534408777ae0b0261
Keiddy <keiddygarcia@gmail.com>
2021-11-26 14:37:53 -0500

    Update canciones.service.js

--- src/canciones/canciones.service.js
+++ src/canciones/canciones.service.js
@@ -3,10 +3,26 @@ const pool = require("../database");
 const cancionesService = {}
 
 cancionesService.obtenerNombres = async () => {
-    const obtenerNombres = await pool.query(
-        'SELECT * FROM nombres'
-    )
-    return obtenerNombres
+    try {
+        const obtenerNombres = await pool.query(
+            'SELECT * FROM nombres'
+        )
+        return obtenerNombres
+    } catch (error) {
+        return error
+    }
+}
+
+cancionesService.usuariosCanciones = async (canciones) => {
+    try {
+        const usuariosCanciones = await pool.query(
+            'INSERT INTO usuarioCanciones SET ?',
+            canciones
+        )
+        return usuariosCanciones
+    } catch (error) {
+        return error
+    }
 }
 
 module.exports = cancionesService;
