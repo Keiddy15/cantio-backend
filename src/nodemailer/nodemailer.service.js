62643376c36592adf1c684859e0509926b8ec7ac
Keiddy <keiddygarcia@gmail.com>
2022-01-13 23:08:13 -0500

    Update nodemailer.service.js

--- src/nodemailer/nodemailer.service.js
+++ src/nodemailer/nodemailer.service.js
@@ -3,13 +3,13 @@ const email = require('./email')
 
 const nodemailerService = {}
 
-nodemailerService.correoConfirmaciónPago = async (email, pass) => {
+nodemailerService.correoConfirmacionPago = async (email, pass) => {
     try {
         await transporter.sendMail({
             from: '"Cantío" <infocantio@gmail.com>',
             to: email,
             subject: "Ha llegado la música",
-            html: email.correoConfirmaciónPago(email, pass)
+            html: email.correoConfirmacionPago(email, pass)
         })
     } catch (error) {
         console.log(error)
